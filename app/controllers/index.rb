203293f56eb1f20746a41987bd83fff2006fadf9
Travis75 <travis.gibson75@gmail.com>
2014-08-28 15:44:51 -0700

    Finished user verification process. Redirect in the post for login still needs to be filled in (index line: somewhere).

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,11 +6,20 @@ get '/login' do
   erb :login_page
 end
 
+post '/create_acct' do
+  User.create(name: params[:username], password: params[:password])
+  redirect to('/')
+end
+
 get '/create_acct' do
   erb :create_acct
 end
 
 post '/login' do
-
-session[:username] = params[:username]
+  if User.exist?(params[:username], params[:password])
+    session[:username] = params[:username]
+    redirect to ('/')
+  else
+    redirect to('/')
+  end
 end
--- app/helpers/formatting.rb
+++ app/helpers/formatting.rb
@@ -2,4 +2,5 @@ helpers do
   def signed_in?
     session[:user_id]
   end
+
 end
