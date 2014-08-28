a1d0297315733f03a3d6b1868f63e5b737ea5a1d
Andrew McClellan <andrew.mcclellan3@gmail.com>
2014-08-28 12:28:51 -0700

    Adds model and migration for tweets * set limit for content to 140

---
+++ db/migrate/2_create_tweets.rb
@@ -0,0 +1,9 @@
+class CreateTweets < ActiveRecord::Migration
+  def change
+    create_table :tweets do |t|
+      t.string :content, limit: 140
+      t.belongs_to :user
+      t.timestamps
+    end
+  end
+end
