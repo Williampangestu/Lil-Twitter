462d47d5ada4521e09350e9db0c61d91abd73dfd
DevReddy <RyanEddyDev@gmail.com>
2014-08-29 14:17:43 -0700

    Changed display of tweets on dashboard to address stolen tweets issue. Also rerouted all user profile pages to begin with /user/ to avoid wrong routing errors.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,36 +5,44 @@ end
 get '/dashboard' do
   if signed_in?
     @user = User.find_by(name: session[:username])
-    @tweets = @user.tweets
-    @user.followees.each do |followee|
-      @tweets << followee.tweets
+    @tweets = []
+    @user.tweets.each do |tweet|
+      @tweets << tweet
     end
-    @tweets.order(created_at: :desc)
+    @user.followees.each do |followee|
+      followee.tweets.each do |tweet|
+        @tweets << tweet
+      end
+    end
+    @tweets.sort! {|a,b| a.created_at <=> b.created_at}
     erb :dashboard
   else
     redirect '/'
   end
 end
 
-get '/:username' do
+get '/user/:username' do
   if signed_in?
     # p User.find_by_name(params[:username])
     # p session[:username]
     @user = User.find_by_name(params[:username]) || User.find_by(name: session[:username])
-    @tweets = @user.tweets
+    @tweets = []
+    @user.tweets.each do |tweet|
+      @tweets << tweet
+    end
     erb :profile_page
   else
     redirect '/'
   end
 end
 
-get '/:username/followings' do
+get '/user/:username/followings' do
   @user = User.find_by_name(params[:username])
   @followings = @user.followees
   erb :following
 end
 
-get '/:username/followers' do
+get '/user/:username/followers' do
   @user = User.find_by_name(params[:username])
   @followers = @user.followers
   erb :follower
